6f5df370f0c15b8751b2faec77e0e485d1049afd
bala-amypo <bala@amypo.in>
2025-12-18 04:41:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studententity.java
+++ src/main/java/com/example/demo/entity/Studententity.java
@@ -15,35 +15,40 @@ public class Studententity{
     private String pass;
     private Date date;
     
-    public Integer getrollno(){
-        return rollno;
-    }
+    
     public void setrollno(Integer rollno){
         this.rollno=rollno;
     }
-    public String getname(){
-        return name;
+    public Integer getrollno(){
+        return rollno;
     }
+   
     public void setname(String name){
         this.name=name;
+    }
+     public String getname(){
+        return name;
+    }
+    
+    public void setemail(String email){
+        this.email=email;
     }
     public String getemail(){
         return email;
     }
-    public void setemail(String email){
-        this.email=email;
+    
+    public void setpass(String pass){
+        this.pass=pass;
     }
     public String getpass(){
         return pass;
     }
-    public void setpass(String pass){
-        this.pass=pass;
+    
+    public void setdate(Date date){
+        this.date=date;
     }
     public Date getdate(){
         return date;
-    }
-    public void setdate(Date date){
-        this.date=date;
     }
     public Studententity(Integer rollno,String name,String email,String pass,Date date){
             this.rollno=rollno;
