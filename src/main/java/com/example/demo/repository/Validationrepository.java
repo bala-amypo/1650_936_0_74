33de97b5cbd29cd564868a981f364fcc90689c84
bala-amypo <bala@amypo.in>
2025-12-18 08:42:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/Validationrepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+@Repository
+public interface Validationrepository extends JpaRepository<Studententity,Integer>{
+
+    
+}
--- src/main/java/com/example/demo/service/Impl/Validserviceimpl.java
+++ src/main/java/com/example/demo/service/Impl/Validserviceimpl.java
@@ -2,5 +2,7 @@ package com.example.demo.service.Impl;
 public Validserviceimpl implements Validservice{
     @Autowired Validationrepository vali;
     @Override
-    
+    public Validationentity posttdata(Validationentity val){
+        return vali.save(val);
+    }
 }
