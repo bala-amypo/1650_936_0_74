1e9f7ee2cad1997bacf9f32d58fff3115950ba4a
bala-amypo <bala@amypo.in>
2025-12-19 07:57:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/TimeStampController.java
+++ src/main/java/com/example/demo/controller/TimeStampController.java
@@ -2,19 +2,19 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import com.example.demo.entity.ValidationEntity;
-import com.example.demo.service.ValidationService;
+import com.example.demo.entity.TimeStamp;
+import com.example.demo.service.TimeStampService;
 // import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
 @RestController
-public class ValidationController{
-  @Autowired ValidationService va;
+public class TimeStampController{
+  @Autowired TimeStampService ti;
   @PostMapping("/postvall")
-  public ValidationEntity sendData(@Valid @RequestBody ValidationEntity val){
-      return va.postData(val);
+  public TimeStamp sendDataa(@Valid @RequestBody TimeStamp ts){
+      return ti.postDataa(ts);
   }
   
 }
