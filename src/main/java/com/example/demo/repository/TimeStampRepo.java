8cc65858f5ca02860329b30c74abede7f6146014
bala-amypo <bala@amypo.in>
2025-12-19 07:48:22 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/TimeStampRepo.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.TimeStamp;
+
+public interface TimeStampRepo extends JpaRepository<TimeStamp,Long>{
+      
+}                                                                                                                                                                                                                                                                                                                                                                                                          
---
+++ src/main/java/com/example/demo/service/TimeStampService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+import com.example.demo.entity.ValidationEntity;
+public interface ValidationService{
+        ValidationEntity postData(ValidationEntity val);
+        ValidationEntity getData(Long id);
+   
+}
