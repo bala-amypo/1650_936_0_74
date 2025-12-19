edde191731daf4368ba1bdcd5d136aaa79d3b0e5
bala-amypo <bala@amypo.in>
2025-12-19 05:01:25 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceimpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceimpl.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.ValidationEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.repository.ValidationRepo;
 import com.example.demo.service.ValidationService;
+import com.example.demo.exception.ValidationException;
 @Service
 public class ValidationServiceimpl implements ValidationService{
   @Autowired ValidationRepo validate;
@@ -14,6 +15,6 @@ public class ValidationServiceimpl implements ValidationService{
       @Override 
      public ValidationEntity getdata(Long id){
       //   return validate.findById(id).orElse(null);
-      return validate.findById(id).orElseThrow(()->)
+      return validate.findById(id).orElseThrow(()->new ValidationException("invalid"+id));
      }
 }    
