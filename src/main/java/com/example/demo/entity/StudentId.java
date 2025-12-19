60c9a350ca0864f179e34f3e1f1b2c09798fea83
bala-amypo <bala@amypo.in>
2025-12-19 10:04:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentId.java
+++ src/main/java/com/example/demo/entity/StudentId.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 public class StudentId{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Integer id;
+    private Integer student_id;
     private String idno;
     private String stuid;
     @OneToOne
