5e86bb13052edb59c87e6c9fbdd88461a6296f5c
bala-amypo <bala@amypo.in>
2025-12-19 07:54:22 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/TimeStampController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.entity.ValidationEntity;
+import com.example.demo.service.ValidationService;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
+@RestController
+public class ValidationController{
+  @Autowired ValidationService va;
+  @PostMapping("/postvall")
+  public ValidationEntity sendData(@Valid @RequestBody ValidationEntity val){
+      return va.postData(val);
+  }
+  
+}
--- src/main/java/com/example/demo/service/Impl/TimeStampServiceimpl.java
+++ src/main/java/com/example/demo/service/Impl/TimeStampServiceimpl.java
@@ -1,16 +1,16 @@
 package com.example.demo.service.Impl;
 import org.springframework.stereotype.Service;
-import com.example.demo.entity.ValidationEntity;
+import com.example.demo.entity.TimeStamp;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.demo.repository.ValidationRepo;
-import com.example.demo.service.ValidationService;
+import com.example.demo.repository.TimeStampRepo;
+import com.example.demo.service.TimeStampService;
 // import com.example.demo.exception.ValidationException;
 @Service
 public class TimeStampServiceimpl implements TimeStampService{
   @Autowired TimeStampRepo stamp;
   @Override
    public TimeStamp postDataa(TimeStamp ti){
-             return validate.save(val);
+             return stamp.save(ti);
       }
       
 }    
