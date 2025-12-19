8309c31eb26320f6888577498290931fead01635
bala-amypo <bala@amypo.in>
2025-12-19 07:24:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/Studentcontroller.java
+++ src/main/java/com/example/demo/controller/Studentcontroller.java
@@ -1,41 +1,37 @@
 package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-import com.example.demo.service.Studentservice;
-import com.example.demo.entity.Studententity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.service.StudentService;
+import com.example.demo.entity.StudentEntity;
 import java.util.List;
 @RestController
-public class Studentcontroller{
-    @Autowired Studentservice ser;
-    @PostMapping("/post")
-    public Studententity senddata(@RequestBody Studententity stu){
-        return ser.postdata(stu);
-    }
-    @GetMapping("/get")
-    public List<Studententity> getdata(){
-        return ser.getalldata();
-    }
-    @DeleteMapping("/delete/{rollno}")
-    public String deleteval(@PathVariable int rollno){
-        return ser.deletedata(rollno);
+public class StudentController{
 
-    }
-    @GetMapping("/getid/{rollno}")
-    public Studententity getdataid(@PathVariable int rollno){
-        return ser.getdata(rollno); 
-    }
-    @PutMapping("/put/{rollno}")
-    public Studententity putval(@PathVariable int rollno,@RequestBody Studententity entity){
-        return ser.updatedata(rollno,entity);
-    }
-     
-
-
-
+  @Autowired StudentService ser;
+  @PostMapping("/post")
+  public StudentEntity sendData(@RequestBody StudentEntity ent){
+      return ser.postData(ent);
+  }
+  @GetMapping("/get")
+  public List<StudentEntity> getval(){
+    return ser.getAllData();
+  }
+  @DeleteMapping("/delete/{id}")
+  public String deleteval(@PathVariable int id){
+  return ser.deleteData(id);
+  }
+  @GetMapping("/getId/{id}")
+  public StudentEntity getDataid(@PathVariable int id){
+    return ser.getData(id);
+  }
+  @PutMapping("/put/{id}")
+  public StudentEntity putval(@PathVariable int id,@RequestBody StudentEntity entity){
+    return ser.updateData(id,entity);
+  }
 }
