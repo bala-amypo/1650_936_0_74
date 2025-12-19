3fa0e9c6d6273612421a8629f38a01d9560465a0
bala-amypo <bala@amypo.in>
2025-12-19 10:46:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StudentIdController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.entity.TimeStamp;
+import com.example.demo.service.TimeStampService;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.validation.Valid;
+@RestController
+public class TimeStampController{
+  @Autowired TimeStampService ti;
+  @PostMapping("/postvall")
+  public TimeStamp sendDataa(@Valid @RequestBody TimeStamp ts){
+      return ti.postDattaa(ts);
+  }
+  
+}     
